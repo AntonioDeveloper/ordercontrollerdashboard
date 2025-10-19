4a732d6935fc75a892082953f1eb6c7477af71c5
Antonio <antonio.junior@original.io>
2025-10-19 00:38:18 -0300

    Reorganized Data Base Tables

--- server/models/order.ts
+++ server/models/order.ts
@@ -1,17 +1,20 @@
 import mongoose from 'mongoose';
 
-const OrderSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  cardId: { type: String, required: true },
-  nome_cliente: { type: String, required: true },
-  status_pedido: { type: String, required: true },
-  endereco: String,
-  pedido: {
-    pizza_sabor: String,
-    tamanho: String,
-    quantidade: Number,
-    observacoes: String,
+const OrderSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true },
+    cardId: { type: String, required: true },
+    nome_cliente: { type: String, required: true },
+    status_pedido: { type: String, required: true },
+    endereco: String,
+    pedido: {
+      pizza_sabor: String,
+      tamanho: String,
+      quantidade: Number,
+      observacoes: String,
+    },
   },
-}, { collection: 'clients' });
+  { collection: 'orders' }
+);
 
 export default mongoose.model('Order', OrderSchema);
