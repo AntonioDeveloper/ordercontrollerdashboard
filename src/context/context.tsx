43ea1432935c690b62f8f2dd017f87ca4c8a6bbf
Antonio <antonio.junior@original.io>
2025-11-05 20:29:07 -0300

    Random order id generation

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -171,11 +171,11 @@ export function OrdersProvider({ children }: { children: ReactNode }) {
     const charArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"];
 
     let cardIdCode: string = "";
-    let assembleArray: Array<string> = [];
     let i = 0;
 
 
     function genCardIdCode() {
+      const assembleArray: string[] = [];
       do {
         assembleArray.push(charArray[Math.floor(Math.random() * charArray.length)])
         i++;
